c85b74f0fbb7c7ddd56289910b0fcd3c2dd4038f
Gabriel Alejandro Rodriguez <garodriguez@baufest.com>
2019-12-10 17:13:36 -0300

    lalalala

---
+++ src/main/Descuento/Descuento.java
@@ -0,0 +1,28 @@
+package Descuento;
+
+public class Descuento {
+    private double C_Anual = 0.2;
+    private double C_Resto = 0.05;
+
+    private double calculardescuento(double monto, double descuento) {
+        return monto - (monto * descuento);
+    }
+
+    public double aplicardescuento(SuscripcionAnual suscripcion) {
+        return this.calculardescuento(suscripcion.calcularcosto(),this.C_Anual);
+    }
+
+    public double aplicardescuento(SuscripcionMensual suscripcion) {
+        return this.calculardescuento(suscripcion.calcularcosto(),this.C_Resto);
+    }
+
+    public double aplicardescuento(NoSuscripto suscripcion) {
+        return this.calculardescuento(suscripcion.calcularcosto(),this.C_Resto);
+    }
+
+    public double aplicardescuento(Factura factura) {
+
+        return this.calculardescuento(factura.calcular(),this.C_Resto);
+    }
+
+}
